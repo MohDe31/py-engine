f26323622f12b6f639c43b4d67730488230c4c38
roumaLch <93671096+roumaLch@users.noreply.github.com>
2022-02-27 23:09:22 +0100

    Update simulation.py

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import core.time
 import glm
 
@@ -9,12 +10,19 @@ class Simulation:
     def __init__(self, resolution: int, type_: str) -> None:
         self.resolution = resolution
         self.type  = type_
+        
+        if self.type == 'r' :
+            self.prey  = glm.vec3(15, 0, -50)
+        elif self.type == 'h' :
+            #make it bigger
+            self.z = np.linspace(-2*np.pi, 2*np.pi, 1000)
+            self.prey = glm.vec3(np.cos(self.z[0]), np.sin(self.z[0]), self.z[0])
+        elif self.type == 'a' :
+            self.prey = glm.vec3(15, 0, 0)
+            self.v = glm.vec3(0, .01, .01)
 
-        self.prey  = glm.vec3(15, 0, -50)
         self.point = glm.vec3(0)
         self.pred  = glm.vec3(0)
-
-        self._lambda = 0
 
         # Speed
         self.alpha = 0.0054
@@ -31,6 +39,17 @@ class Simulation:
         # Movement rectiligne
         if self.type == 'r':
             self.prey += glm.vec3(0, core.time.Time.FIXED_DELTA_TIME, 0)
+        # Movement helicoidale 
+        if self.type == 'h':
+            self.z += 2*np.pi/1000
+            self.prey += glm.vec3(np.cos(self.z[self.iteration+2]), np.sin(self.z[self.iteration+2]), self.z[self.iteration+2])
+        if self.type == 'a' :
+            self.prey += self.v
+            if self.iteration % 10 == 0 :
+                axis = [glm.vec3(1, 0, 0), glm.vec3(0, 1, 0), glm.vec3(0, 0, 1)]
+                np.random.shuffle(axis)
+                self.v = glm.rotate(self.v, np.pi/9, axis[0])
+
 
 
     def run(self):
