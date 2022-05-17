cc9f7963d4bab0217c77f9de1a0debbea713e946
jae <jae@pop-os.localdomain>
2022-05-17 21:34:07 +0100

    add cube color

--- core/primitives.py
+++ core/primitives.py
@@ -26,7 +26,7 @@ def line(scene: core.scene.Scene, start, end, color = [0, 0, 0, 1]):
     return line_
 
 
-def cube(scene: core.scene.Scene, position, global_scale=1):
+def cube(scene: core.scene.Scene, position, color = [0, 0, 0, 1], global_scale=1):
     cube_ = scene.makeEntity()
     mesh_: core.components.mesh.Mesh = cube_.addComponent(core.components.mesh.Mesh)
     tr_ = cube_.addComponent(core.components.transform.Transform, *position, *([0]*3))
@@ -46,14 +46,14 @@ def cube(scene: core.scene.Scene, position, global_scale=1):
                                  0.5,  0.5, -0.5, 
                                  -0.5,  0.5, -0.5], dtype=np.float32)*global_scale
 
-    mesh_.m_Colors = np.array([1.0, 0.0, 0.0, 1.0,
-                               0.0, 1.0, 0.0, 1.0,
-                               0.0, 0.0, 1.0, 1.0,
-                               1.0, 1.0, 1.0, 1.0,
-                               1.0, 0.0, 0.0, 1.0,
-                               0.0, 1.0, 0.0, 1.0,
-                               0.0, 0.0, 1.0, 1.0,
-                               1.0, 1.0, 1.0, 1.0], dtype=np.float32)
+    mesh_.m_Colors = np.array([*color,
+                               *color,
+                               *color,
+                               *color,
+                               *color,
+                               *color,
+                               *color,
+                               *color], dtype=np.float32)
 
     mesh_.buildMesh()
 
